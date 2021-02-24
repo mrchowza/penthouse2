8b93cc03ccf054560e376f09be4c0b871ebb0929
mrchowza <79600444+mrchowza@users.noreply.github.com>
2021-02-24 11:45:34 -0700

    Update panel-header.js

--- client/src/containers/panel-header.js
+++ client/src/containers/panel-header.js
@@ -5,7 +5,7 @@ function PanelHeader() {
     return (
 
 <nav className="navbar navbar-expand navbar-dark bg-dark">
-  <h1 className="" href="#">Steambotauto</h1>
+  <h1 className="" href="#">xIdle</h1>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
